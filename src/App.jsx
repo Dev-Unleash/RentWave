fbcb387d8932872c24e932a79c0cd9cdb5d35f38
Sanni-Damilola <sannifortune11@gmail.com>
2024-09-08 00:07:57 +0100

    updated tenant dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -12,77 +12,70 @@ import Login from './Components/Auth/Login';
 import Signup from './Components/Auth/Signup';
 import ResetPassword from './Components/Auth/ResetPassword';
 import ForgotPassword from './Components/Auth/ForgotPassword';
-import Tenant from "./Pages/Tenant/Tenant.jsx"
+// import Tenant from "./Pages/Tenant/Tenant.jsx"
 import TenantPrivate from './Pages/Tenant/TenantPrivate.jsx';
 import Landlord from "./Pages/Landlord/Landlord.jsx"
 import Admin from "./Pages/Admin/Admin.jsx"
 import AdminPrivate from './Pages/Admin/AdminPrivate.jsx';
-
+import TenantHome from './Pages/Tenant/TenantHome.jsx';
+import TenantSettings from './Pages/Tenant/TenantSettings.jsx';
+import TenantPayment from './Pages/Tenant/TenantPayment.jsx';
 
 const router = createBrowserRouter([
-{  path:"/Tenant",
-  element:<Tenant/>},
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/Login",
+    element: <Login/>
+  },
+  {
+    path: "/Signup",
+    element: <Signup/>
+  },
+  {
+    path: "/ResetPassword",
+    element: <ResetPassword/>
+  },
+  {
+    path: "/ForgotPassword",
+    element: <ForgotPassword/>
+  },
+  {
+    path: "/Contact_us",
+    element: <Contact_us/>
+  },
+  {
+    path: "/Services",
+    element: <Services/>
+  },
+  {
+    path: "/Blog",
+    element: <Blog/>
+  },
+  {
+    path: "/Landlord",
+    element: <Landlord/>
+  },
+  {
+   
+    element: <TenantPrivate/>,
+    children: [
       {
-        path: "/",
-        element: <Home/>
+        path: "/TenantHome",
+        element: <TenantHome/>
       },
       {
-        path:"/Login",
-        element:<Login/>
+        path: "/TenantSetting",
+        element: <TenantSettings/>
       },
-      {
-        path:"/Signup",
-        element:<Signup/>
-      },
-      {
-        path:"/ResetPassword",
-        element:<ResetPassword/>
-      },{
-        path:"ForgotPassWord",
-        element:<ForgotPassword/>
-      },
-      {
-        path: "/Contact_us",
-        element: <Contact_us/>
-      },
-      {
-        path: "/Services",
-        element: <Services/>
-      },
-      {
-        path: "/Blog",
-        element: <Blog/>
-      },
-      {
-            
-        path:"/Landlord",
-        element:<Landlord/>
-       },
-      {
-        element:<AdminPrivate/>,
-        children:[
-          { path:"/Admin",
-            element:<Admin/>
-           },
-        ],
-      },
-      {
-        element:<AuthRoutes/>,
-        children:[
-          // {
-            
-          //     path:"/Landlord",
-          //     element:<Landlord/>
-          //    },
-          
-        ]
-      },
-     
-    ],
-  
-  
-  
-);
+      {path:"/TenantPayment",
+      element:<TenantPayment/>
+    }
+    ]
+  }
+]);
 
 const App = () => {
   return(
