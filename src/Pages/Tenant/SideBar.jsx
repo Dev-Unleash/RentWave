8d62ff7160749c6a01828d2ecc7c41dcd7235289
Sanni-Damilola <sannifortune11@gmail.com>
2024-09-15 01:36:21 +0100

    logout popup

--- src/Pages/Tenant/SideBar.jsx
+++ src/Pages/Tenant/SideBar.jsx
@@ -1,61 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import ola from "../../assets/ola.jpg";
 import { CiStar } from "react-icons/ci";
 import { CiWallet } from "react-icons/ci";
 import { IoHome } from "react-icons/io5";
 import { AiOutlineLogout } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../assets/logo-removebg-preview.png";
 import "./Tenant.css";
-const SideBar = () => {
+
+const SideBar = ({showLogoutPopup,setShowLogoutPopup}) => {
+  const nav =useNavigate()
+  const handleLogoutClick = () => {
+    setShowLogoutPopup(!showLogoutPopup);
+  };
+  const handleLogoutConfirm = () => {
+    nav("/")
+  };
+  const handleLogoutCancel = () => {
+    setShowLogoutPopup(false);
+  };
+
   return (
-    <>
-      <div className="LogoContainer">
-        <div className="Logo">
-          <img src={Logo} alt="" />
+    <div className="Sidebarwhole">
+      <Link to="/">
+        <div className="LogoContainer">
+          <div className="Logo">
+            <img src={Logo} alt="Logo" />
+          </div>
         </div>
-      </div>
+      </Link>
 
       <div className="Profile">
         <div className="Pics">
-          <img src={ola} alt="" />
+          <img src={ola} alt="Profile" />
         </div>
         <p>Tolani</p>
         <h3>Welcome</h3>
       </div>
-      <div className="MenuContainer" >
+
+      <div className="MenuContainer">
         <div className="MenuWrapper">
           <nav>
-            {" "}
-            <IoHome className="menuIcon"/>
-            <Link to="/TenantHome" style={{ textDecoration: "none" ,color:"black" ,display:"flex",alignItems:"center" }}>
+            <IoHome className="menuIcon" />
+            <Link to="/TenantHome" style={{ textDecoration: "none", color: "black", display: "flex", alignItems: "center" }}>
               Home
-            </Link>{" "}
-          </nav>
-          <nav>
-            <CiWallet className="menuIcon" />
-            <Link to="/TenantSub" style={{ textDecoration: "none",color:"black" ,display:"flex",alignItems:"center"  }}>
-              Subcription
             </Link>
           </nav>
           <nav>
-            {" "}
-            <CiStar className="menuIcon" />{" "}
-            <Link to="/TenantMain" style={{ textDecoration: "none",color:"black",display:"flex",alignItems:"center"  }}>
+            <CiWallet className="menuIcon" />
+            <Link to="/TenantSub" style={{ textDecoration: "none", color: "black", display: "flex", alignItems: "center" }}>
+              Subscription
+            </Link>
+          </nav>
+          <nav>
+            <CiStar className="menuIcon" />
+            <Link to="/TenantMain" style={{ textDecoration: "none", color: "black", display: "flex", alignItems: "center" }}>
               Maintenance
             </Link>
           </nav>
           <nav>
-            {" "}
-            <CiWallet className="menuIcon" />{" "}
-            <Link to="/TenantPayment" style={{ textDecoration: "none",color:"black",display:"flex",alignItems:"center"  }}>
+            <CiWallet className="menuIcon" />
+            <Link to="/TenantPayment" style={{ textDecoration: "none", color: "black", display: "flex", alignItems: "center" }}>
               Payment
             </Link>
           </nav>
           <nav>
-            <CiStar className="menuIcon" />{" "}
-            <Link to="/TenantSettings" style={{ textDecoration: "none",color:"black",display:"flex",alignItems:"center" }} >
-              {" "}
+            <CiStar className="menuIcon" />
+            <Link to="/TenantSettings" style={{ textDecoration: "none", color: "black", display: "flex", alignItems: "center" }}>
               Account Setting
             </Link>
           </nav>
@@ -63,16 +74,24 @@ const SideBar = () => {
       </div>
 
       <div className="Logoutmenu">
-        
-        <nav style={{gap:"20px",display:"flex"}}>
-            <AiOutlineLogout className="menuIcon"/>
-            <Link to="/TenantSettings" style={{ textDecoration: "none",color:"black",fontSize:"20px",fontWeight:'500',display:"flex",alignItems:"center"  }} >
-              {" "}
-              Logout
-            </Link>
-          </nav>
+        <nav style={{ gap: "20px", display: "flex" }} onClick={handleLogoutClick}>
+          <AiOutlineLogout className="menuIcon" />
+          <p style={{ fontSize: "25px", color: "black", display: "flex", alignItems: "center", cursor: "pointer" }}>
+            Logout
+          </p>
+        </nav>
+
+        {showLogoutPopup && (
+          <div className="popup">
+            <p style={{color:"white",width:"100%",display:"flex",justifyContent:"center"}}>Are you sure?</p>
+            <div className="popup-text">
+              <p onClick={handleLogoutConfirm}>Yes</p>
+              <p onClick={handleLogoutCancel}>No</p>
+            </div>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/Pages/Tenant/TenantPrivate.jsx
+++ src/Pages/Tenant/TenantPrivate.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import SideBar from './SideBar'
 import "./Tenant.css"
-const TenantPrivate = () => {
+const TenantPrivate = ({showLogoutPopup,setShowLogoutPopup}) => {
   return (
     
-      <div className='tenantPage'>
+      <div className="tenantPage">
         <div className="tenantLeft">
-          <SideBar />
+          <SideBar showLogoutPopup={showLogoutPopup} setShowLogoutPopup={setShowLogoutPopup}/>
         </div>
-        <div className="tenantRight">
+        <div className="tenantRight" >
           <div className="tenantRightWrapper">
             <Outlet /> {/* This will render the nested routes */}
           </div>
