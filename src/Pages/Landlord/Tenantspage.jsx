c8d5804344c6d9893ae63e108e6acc898d5372f2
Michael <mickeygodwin07@gmail.com>
2024-09-18 13:30:14 -0700

    modified:   src/Pages/Landlord/Tenantspage.jsx

--- src/Pages/Landlord/Tenantspage.jsx
+++ src/Pages/Landlord/Tenantspage.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react'
 import './Tenantspage.css'
-<<<<<<< HEAD
 import image from "../../../public/Clip.png"
 import InviteTenant from './InviteTenant'
-=======
 import image from "../../assets/Clip.png"
->>>>>>> 3a0ee0ce391d30b02a0ae03102389ffc51010ed1
 
 const Tenantspage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -23,12 +20,9 @@ const Tenantspage = () => {
         </div>
         <div className="btnes">
             <p>View your tenants</p>
-<<<<<<< HEAD
             <botton onClick={openModal}>Invite Tenants</botton>
-=======
             <botton className="btnsLandlord">Invite Tenants</botton>
->>>>>>> 3a0ee0ce391d30b02a0ae03102389ffc51010ed1
-          </div>
+        </div>
         <div className="table">
           <img src={image} alt="" />
           <p>No renters yet but renter details and status will appear here.</p>
