2399cc1d26caf7beecfee208a8fe435832fba5c4
Sanni-Damilola <sannifortune11@gmail.com>
2024-09-20 19:03:20 +0100

    update

--- src/Pages/Tenant/TenantProfile.jsx
+++ src/Pages/Tenant/TenantProfile.jsx
@@ -11,7 +11,10 @@ const TenantProfile = () => {
   };
 
   const tenantData = JSON.parse(localStorage.getItem("userProfile"))?.tenant;
-
+  // const userToken = localStorage.getItem("userToken");
+  const userInfo = localStorage.getItem("userInfo");
+  const userData = JSON.parse(userInfo);
+  console.log(userData)
   return (
     <div className="TenantProfilePage">
       <div className="TenantProfilePageWrapper">
