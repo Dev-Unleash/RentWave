6cb6eed8e4f0a795c4bd46ef3fb7574be57b2b69
Dev-Unleash <darthroy18@gmail.com>
2024-09-25 23:56:46 +0100

    updated

--- src/Pages/Landlord/Tenantspage.jsx
+++ src/Pages/Landlord/Tenantspage.jsx
@@ -140,7 +140,7 @@ const Tenantspage = () => {
         </div>
         <div className="tableland">
           <div className="tenantTable">
-            <table className="tenant-table">
+            <table className="landlord-table">
               <thead>
                 <tr>
                   <th className="name-column">Name</th>
